a1cacd3209d27652e25d381cf8e70a1a491f02bb
thiagobcf <thiagobcfonseca@gmail.com>
2023-09-11 18:48:43 -0300

    viewset

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,9 +1,8 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
-from rest_framework.generics import (ListCreateAPIView,
-                                     RetrieveUpdateDestroyAPIView)
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
 from tag.models import Tag
 
@@ -15,18 +14,12 @@ class RecipeAPIv2Pagination(PageNumberPagination):
     page_size = 2
 
 
-class RecipeAPIv2List(ListCreateAPIView):
+class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
 
-
-class RecipeAPIv2Detail(RetrieveUpdateDestroyAPIView):
-    queryset = Recipe.objects.get_published()
-    serializer_class = RecipeSerializer
-    pagination_class = RecipeAPIv2Pagination
-
-    def patch(self, request, *args, **kwargs):
+    def partial_update(self, request, *args, **kwargs):
         pk = kwargs.get('pk')
 
         recipe = self.get_queryset().filter(pk=pk).first()
